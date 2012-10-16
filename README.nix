81a61338f29b077e6ada9082191d4da8f5ea266e
Caio Marcelo de Oliveira Filho <caio.oliveira@openbossa.org>
2012-10-16 15:49:41 -0300

    Making README.nix more clear
    Also removing extra freetype in deps list since we automatically
    download it using jhbuild (via update-webkitnix-libs).
    
    Reviewed by Lauro Neto.

--- README.nix
+++ README.nix
@@ -7,10 +7,12 @@ GLX is used by default, to use EGL pass --egl to build-webkit script.
 Packages required
 =================
 
-In order to compile Nix on the buildbot, the following packages were
-installed:
+WebKitNix requires some packages in the system as well as
+automatically pull other dependencies using the jhbuild tool.
 
-For jhbuild, following http://trac.webkit.org/wiki/BuildingGtk:
+To compile WebKitNix all the three groups of packages below are necessary:
+
+# These are needed for jhbuild tool to work.
 $ sudo apt-get install gobject-introspection \
                        icon-naming-utils \
                        libgcrypt11-dev \
@@ -19,15 +21,14 @@ $ sudo apt-get install gobject-introspection \
                        libtiff4-dev \
                        libcroco3-dev
 
-For WebKitNix dependencies (automatically pulled by jhbuild)
+# These are needed for the dependencies that are automatically compiled.
 $ sudo apt-get install gettext \
                        libpng12-dev \
-                       libfreetype6-dev \
                        g++\
                        gtk-doc-tools \
                        libgnutls-dev
 
-For WebKitNix itself
+# These are needed for WebKitNix itself.
 $ sudo apt-get install cmake \
                        flex bison gperf \
                        libsqlite3-dev \
@@ -38,6 +39,10 @@ $ sudo apt-get install cmake \
 NOTE: the package libgl1-mesa-dev can be replaced by another that
 provides the OpenGL headers (GLES-based systems, for example).
 
-Other dependencies like glib, cairo et al. are pulled by jhbuild.
+Other dependencies like glib, cairo et al. are pulled by jhbuild, by
+using the script:
+
+$ Tools/Scripts/update-webkitnix-libs
+
 They are compiled and installed into WebKitBuild/Dependencies.
 
