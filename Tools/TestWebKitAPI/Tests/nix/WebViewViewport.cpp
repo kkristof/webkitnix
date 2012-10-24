05f6421387c8f98fbe2e3137945525d7f7a10d2e
Hugo Parente Lima <hugo.lima@openbossa.org>
2012-10-24 19:29:56 -0300

    Apply forgoten suggestions for commit f4fb6ff3b701ac0ee7b4ed2d702d4acdd3f05ffd.
    Reviewed by: Caio Marcelo de Oliveira Filho

--- Tools/TestWebKitAPI/Tests/nix/WebViewViewport.cpp
+++ Tools/TestWebKitAPI/Tests/nix/WebViewViewport.cpp
@@ -12,12 +12,10 @@ TEST(WebKitNix, WebViewViewport)
 {
     using namespace WebKit;
 
-    /*
-     This test opens a page with contents size of 20000x980 pixels, a white
-     background, no viewport metatag and two black dots located at
-     10000x400 and 10400x800, then it scales and scroll the page to check if
-     the black pixels are where they are expected to be.
-     */
+    // This test opens a page with contents size of 20000x980 pixels, a white
+    // background, no viewport metatag and two black dots located at
+    // 10000x400 and 10400x800, then it scales and scroll the page to check if
+    // the black pixels are where they are expected to be.
 
     WKRetainPtr<WKContextRef> context = adoptWK(WKContextCreate());
     Util::ForceRepaintClient client;
@@ -29,7 +27,8 @@ TEST(WebKitNix, WebViewViewport)
 
     Util::PageLoader loader(webView.get());
 
-    const unsigned width = 490; // 980 / 2
+    const unsigned contentsWidth = 980;
+    const unsigned width = contentsWidth / 2;
     const unsigned height = 200;
     webView->setSize(width, height);
 
@@ -40,8 +39,8 @@ TEST(WebKitNix, WebViewViewport)
     loader.waitForLoadURLAndRepaint("../nix/WebViewViewport");
     unsigned char sample[4];
 
-    // All white
-    glReadPixels(0, 199, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, &sample);
+    // All white.
+    glReadPixels(0, height - 1, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, &sample);
     EXPECT_EQ(0xFF, sample[0]);
     EXPECT_EQ(0xFF, sample[1]);
     EXPECT_EQ(0xFF, sample[2]);
@@ -52,15 +51,14 @@ TEST(WebKitNix, WebViewViewport)
     loader.forceRepaint();
 
     // The black dot should be on 0,0
-    glReadPixels(0, 199, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, &sample);
+    glReadPixels(0, height - 1, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, &sample);
     EXPECT_EQ(0x00, int(sample[0]));
     EXPECT_EQ(0x00, int(sample[1]));
     EXPECT_EQ(0x00, int(sample[2]));
     EXPECT_EQ(0xFF, int(sample[3]));
 
     // And another black dot on 200, 200
-    // See the HTML file for more info
-    glReadPixels(200, 0, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, &sample);
+    glReadPixels(height, 0, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, &sample);
     EXPECT_EQ(0x00, int(sample[0]));
     EXPECT_EQ(0x00, int(sample[1]));
     EXPECT_EQ(0x00, int(sample[2]));
