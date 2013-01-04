f6fc8f250fbc28e04fc971f7d87ca86f4981e219
Hugo Parente Lima <hugo.lima@openbossa.org>
2013-01-04 15:45:26 -0300

    Cleanup OptionsNix.cmake.
    Instead of add -DDEFINE=Value to compiler command line, add those defines to cmakeconfig.h
    
    Reviewed By: Caio Marcelo de Oliveira Filho

--- Source/cmake/OptionsNix.cmake
+++ Source/cmake/OptionsNix.cmake
@@ -3,6 +3,23 @@ set(PROJECT_VERSION_MINOR 1)
 set(PROJECT_VERSION_PATCH 0)
 set(PROJECT_VERSION ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH})
 
+set(SHARED_CORE 0)
+set(ENABLE_WEBKIT 0)
+set(ENABLE_WEBKIT2 1)
+
+set(JSC_EXECUTABLE_NAME jsc)
+set(WTF_LIBRARY_NAME wtf_nix)
+set(JavaScriptCore_LIBRARY_NAME javascriptcore_nix)
+set(WebCore_LIBRARY_NAME webcore_nix)
+set(WebKit2_LIBRARY_NAME WebKitNix)
+
+# Macro used to define flags that make non sense to be undefined, e.g. ENABLE_GLIB_SUPPORT on Nix
+set(_WEBKIT_HARDCODED_DEFINES "")
+macro(SET_HARDCODED_DEFINE _name)
+    list(APPEND _WEBKIT_HARDCODED_DEFINES ${_name})
+    set(${_name} 1)
+endmacro()
+
 # Set default values for features based on Features${PORT}.config
 file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/Source/cmake/Features${PORT}.config" featureData NEWLINE_CONSUME)
 string(REPLACE ";" "\\\\;" featureData "${featureData}")
@@ -25,13 +42,17 @@ set(WTF_PLATFORM_NIX 1)
 
 find_package(Cairo 1.10 REQUIRED)
 find_package(Fontconfig 2.8.0 REQUIRED)
-find_package(Sqlite REQUIRED)
+find_package(Freetype REQUIRED)
+find_package(GLIB 2.31.8 REQUIRED COMPONENTS gio gobject gmodule gthread)
+find_package(HarfBuzz REQUIRED)
+find_package(ICU REQUIRED)
+find_package(JPEG REQUIRED)
+find_package(LibSoup 2.39.4.1 REQUIRED)
 find_package(LibXml2 2.6 REQUIRED)
 find_package(LibXslt 1.1.7 REQUIRED)
-find_package(ICU REQUIRED)
-find_package(Threads REQUIRED)
-find_package(JPEG REQUIRED)
 find_package(PNG REQUIRED)
+find_package(Sqlite REQUIRED)
+find_package(Threads REQUIRED)
 find_package(ZLIB REQUIRED)
 
 if (WTF_USE_OPENGL_ES_2)
@@ -41,43 +62,19 @@ else ()
     find_package(OpenGL REQUIRED)
 endif ()
 
-find_package(GLIB 2.31.8 REQUIRED COMPONENTS gio gobject gmodule gthread)
-find_package(LibSoup 2.39.4.1 REQUIRED)
-add_definitions(-DENABLE_GLIB_SUPPORT=1)
-
-set(SHARED_CORE 0)
-
-set(ENABLE_WEBKIT 0)
-set(ENABLE_WEBKIT2 1)
-
-set(WTF_USE_SOUP 1)
-add_definitions(-DWTF_USE_SOUP=1)
-
-add_definitions(-DENABLE_CONTEXT_MENUS=0)
-
-set(WTF_USE_PTHREADS 1)
-add_definitions(-DWTF_USE_PTHREADS=1)
-
-set(WTF_USE_ICU_UNICODE 1)
-add_definitions(-DWTF_USE_ICU_UNICODE=1)
-
-set(WTF_USE_CAIRO 1)
-add_definitions(-DWTF_USE_CAIRO=1)
-
-set(JSC_EXECUTABLE_NAME jsc)
-
-set(WTF_LIBRARY_NAME wtf_nix)
-set(JavaScriptCore_LIBRARY_NAME javascriptcore_nix)
-set(WebCore_LIBRARY_NAME webcore_nix)
-set(WebKit2_LIBRARY_NAME WebKitNix)
-
-
-find_package(Freetype REQUIRED)
-find_package(HarfBuzz REQUIRED)
-set(WTF_USE_FREETYPE 1)
-set(WTF_USE_HARFBUZZ_NG 1)
-add_definitions(-DWTF_USE_FREETYPE=1)
-add_definitions(-DWTF_USE_HARFBUZZ_NG=1)
+SET_HARDCODED_DEFINE(ENABLE_GLIB_SUPPORT)
+SET_HARDCODED_DEFINE(WTF_USE_3D_GRAPHICS)
+SET_HARDCODED_DEFINE(WTF_USE_ACCELERATED_COMPOSITING)
+SET_HARDCODED_DEFINE(WTF_USE_CAIRO)
+SET_HARDCODED_DEFINE(WTF_USE_COORDINATED_GRAPHICS)
+SET_HARDCODED_DEFINE(WTF_USE_FREETYPE)
+SET_HARDCODED_DEFINE(WTF_USE_HARFBUZZ_NG)
+SET_HARDCODED_DEFINE(WTF_USE_ICU_UNICODE)
+SET_HARDCODED_DEFINE(WTF_USE_PTHREADS)
+SET_HARDCODED_DEFINE(WTF_USE_SOUP)
+SET_HARDCODED_DEFINE(WTF_USE_TEXTURE_MAPPER)
+SET_HARDCODED_DEFINE(WTF_USE_TEXTURE_MAPPER_GL)
+SET_HARDCODED_DEFINE(WTF_USE_TILED_BACKING_STORE)
 
 if (NOT ENABLE_SVG)
     set(ENABLE_SVG_FONTS 0)
@@ -104,11 +101,8 @@ if (ENABLE_VIDEO OR ENABLE_WEB_AUDIO)
     find_package(GStreamer REQUIRED COMPONENTS ${GSTREAMER_COMPONENTS})
 endif ()
 
-if (WTF_USE_OPENGL_ES_2)
-    add_definitions(-DWTF_USE_OPENGL_ES_2=1)
-else ()
-    set(WTF_USE_OPENGL 1)
-    add_definitions(-DWTF_USE_OPENGL=1)
+if (NOT WTF_USE_OPENGL_ES_2)
+    SET_HARDCODED_DEFINE(WTF_USE_OPENGL)
 endif ()
 
 if (WTF_USE_EGL)
@@ -116,21 +110,6 @@ if (WTF_USE_EGL)
 endif ()
 
 set(CPACK_SOURCE_GENERATOR TBZ2)
-
-set(WTF_USE_ACCELERATED_COMPOSITING 1)
-add_definitions(-DWTF_USE_ACCELERATED_COMPOSITING=1)
-
-set(WTF_USE_COORDINATED_GRAPHICS 1)
-add_definitions(-DWTF_USE_COORDINATED_GRAPHICS=1)
-
-set(WTF_USE_TEXTURE_MAPPER 1)
-add_definitions(-DWTF_USE_TEXTURE_MAPPER=1)
-
-set(WTF_USE_TEXTURE_MAPPER_GL 1)
-add_definitions(-DWTF_USE_TEXTURE_MAPPER_GL=1)
-
-set(WTF_USE_3D_GRAPHICS 1)
-add_definitions(-DWTF_USE_3D_GRAPHICS=1)
 
 macro(PROCESS_WEBKIT_OPTIONS)
     # Show all options on screen
@@ -178,9 +157,17 @@ macro(PROCESS_WEBKIT_OPTIONS)
     file(WRITE ${CMK_CONFIG_H_IN}
         "#ifndef CMAKECONFIG_H\n"
         "#define CMAKECONFIG_H\n\n")
+
+    file(APPEND ${CMK_CONFIG_H_IN} "\n// Hardcoded defines\n\n")
+    foreach (_name ${_WEBKIT_HARDCODED_DEFINES})
+        file(APPEND ${CMK_CONFIG_H_IN} "#define ${_name} 1\n")
+    endforeach ()
+
+    file(APPEND ${CMK_CONFIG_H_IN} "\n// WebKit options\n\n")
     foreach (_name ${_WEBKIT_AVAILABLE_OPTIONS})
         file(APPEND ${CMK_CONFIG_H_IN} "#cmakedefine01 ${_name}\n")
     endforeach ()
+
     file(APPEND ${CMK_CONFIG_H_IN} "\n#endif // CMAKECONFIG_H\n")
 
     file(MD5 ${CMK_CONFIG_H_IN} CMK_CONFIG_H_IN_NEW_MD5)
