34b7c191fa5ff7f66eeaa21a7f0e9f912ffe4794
Hugo Parente Lima <hugo.lima@openbossa.org>
2013-04-04 18:11:59 -0300

    Nice button, radio and checkboxes.
    Reviewed by: Jesus Sanchez-Palencia

--- Source/Platform/nix/src/DefaultWebThemeEngine.cpp
+++ Source/Platform/nix/src/DefaultWebThemeEngine.cpp
@@ -1,9 +1,42 @@
+/*
+ * Copyright (C) 2013 Nokia Corporation and/or its subsidiary(-ies).
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "config.h"
 
 #include "DefaultWebThemeEngine.h"
 #include "public/WebRect.h"
 #include "public/WebSize.h"
+#include <algorithm>
+#include <cmath>
 #include <cairo/cairo.h>
+
+const double BG_COLOR1 = 0xF6/256.0;
+const double BG_COLOR2 = 0xDE/256.0;
+const double BORDER_COLOR = 0xA4/256.0;
+const double BORDER_ONHOVER_COLOR = 0x7A/256.0;
+const double CHECK_COLOR = 0x66/256.0;
 
 namespace WebKit {
 
@@ -16,9 +49,40 @@ static void notImplemented(WebCanvas* canvas, const WebRect& rect, bool blue = f
     cairo_restore(canvas);
 }
 
+static void gradientFill(cairo_t* cairo, double yStart, double yLength, bool inverted = false)
+{
+    double gradStartColor = BG_COLOR1;
+    double gradEndColor = BG_COLOR2;
+    if (inverted)
+        std::swap(gradStartColor, gradEndColor);
+
+    cairo_pattern_t* gradient = cairo_pattern_create_linear(0, yStart, 0, yStart + yLength);
+    cairo_pattern_add_color_stop_rgb(gradient, 0, gradStartColor, gradStartColor, gradStartColor);
+    cairo_pattern_add_color_stop_rgb(gradient, 1, gradEndColor, gradEndColor, gradEndColor);
+    cairo_set_source(cairo, gradient);
+    cairo_fill(cairo);
+    cairo_pattern_destroy(gradient);
+}
+
+static void setupBorder(cairo_t * cairo, WebThemeEngine::State state)
+{
+    double borderColor = state == WebThemeEngine::StateHover ? BORDER_ONHOVER_COLOR : BORDER_COLOR;
+    cairo_set_source_rgb(cairo, borderColor, borderColor, borderColor);
+    cairo_set_line_width(cairo, 1);
+}
+
 void DefaultWebThemeEngine::paintButton(WebCanvas* canvas, State state, const WebRect& rect, const ButtonExtraParams&) const
 {
-    notImplemented(canvas, rect);
+    cairo_save(canvas);
+    setupBorder(canvas, state);
+    // Cairo uses a coordinate system not based on pixel coordinates, so
+    // we need to add  0.5 to x and y coord or the line will stay between
+    // two pixels instead of in the middle of a pixel.
+    cairo_rectangle(canvas, rect.x + 0.5, rect.y + 0.5, rect.width, rect.height);
+    cairo_stroke_preserve(canvas);
+
+    gradientFill(canvas, rect.y, rect.height, state == StatePressed);
+    cairo_restore(canvas);
 }
 
 void DefaultWebThemeEngine::paintTextField(WebCanvas* canvas, State state, const WebRect& rect) const
@@ -38,7 +102,25 @@ WebSize DefaultWebThemeEngine::getCheckboxSize() const
 
 void DefaultWebThemeEngine::paintCheckbox(WebCanvas* canvas, State state, const WebRect& rect, const ButtonExtraParams& param) const
 {
-    notImplemented(canvas, rect, param.checked);
+    cairo_save(canvas);
+    setupBorder(canvas, state);
+    cairo_rectangle(canvas, rect.x + 0.5, rect.y + 0.5, rect.width, rect.height);
+    cairo_stroke_preserve(canvas);
+
+    gradientFill(canvas, rect.y, rect.height, state == StatePressed);
+
+    if (param.checked) {
+        const double border = 3;
+        cairo_set_line_width(canvas, 2);
+        cairo_set_source_rgb(canvas, CHECK_COLOR, CHECK_COLOR, CHECK_COLOR);
+        cairo_move_to(canvas, rect.x + 0.5 + border, rect.y + 0.5 + rect.height - border);
+        cairo_rel_line_to(canvas, rect.width - border * 2, -rect.height + border * 2);
+        cairo_move_to(canvas, rect.x + 0.5 + border, rect.y + 0.5 + border);
+        cairo_rel_line_to(canvas, rect.width - border * 2, rect.height - border * 2);
+        cairo_stroke(canvas);
+    }
+
+    cairo_restore(canvas);
 }
 
 WebSize DefaultWebThemeEngine::getRadioSize() const
@@ -48,12 +130,30 @@ WebSize DefaultWebThemeEngine::getRadioSize() const
 
 void DefaultWebThemeEngine::paintRadio(WebCanvas* canvas, State state, const WebRect& rect, const ButtonExtraParams& param) const
 {
-    notImplemented(canvas, rect, param.checked);
+    cairo_save(canvas);
+    setupBorder(canvas, state);
+    cairo_arc(canvas, rect.x + rect.width/2.0, rect.y + rect.height/2.0, rect.width/2.0, 0, 2 * M_PI);
+    cairo_stroke_preserve(canvas);
+
+    gradientFill(canvas, rect.y, rect.height);
+
+    if (param.checked) {
+        cairo_set_source_rgb(canvas, CHECK_COLOR, CHECK_COLOR, CHECK_COLOR);
+        cairo_arc(canvas, rect.x + rect.width/2.0, rect.y + rect.height/2.0, rect.width/4.0, 0, 2 * M_PI);
+        cairo_fill(canvas);
+    }
+    cairo_restore(canvas);
 }
 
-void DefaultWebThemeEngine::paintMenuList(WebCanvas* canvas, State, const WebRect& rect) const
+void DefaultWebThemeEngine::paintMenuList(WebCanvas* canvas, State state, const WebRect& rect) const
 {
-    notImplemented(canvas, rect);
+    cairo_save(canvas);
+    setupBorder(canvas, state);
+    cairo_rectangle(canvas, rect.x + 0.5, rect.y + 0.5, rect.width, rect.height);
+    cairo_stroke_preserve(canvas);
+
+    gradientFill(canvas, rect.y, rect.height, state == StatePressed);
+    cairo_restore(canvas);
 }
 
 }
